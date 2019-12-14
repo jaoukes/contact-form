2797415f8012eba123ec3fb48a97e0fdf4b32a82
jessicaoukes <jessicaoukes@gmail.com>
2019-12-14 15:41:06 -0500

    error handler / a route

--- server/index.js
+++ server/index.js
@@ -9,12 +9,17 @@ let port = 3000;
 app.use(express.json()); // Allows to read JSON sent in 'req.body'
 
 // Routes go here
-
-// Default Error Handler
-app.use(function (req, res, next, error) {
-
+router.post('/' function (req, res) {
+  console.log(req.body.b);
+  res.send('Hello Wolrd!');
 });
 
+// Default Error Handler
+// This is always last!
+app.use(function (error, req, res, next) {
+  console.error(error); // this is here to see errors for debugging purposes
+  res.sendStatus(500); // Send 500 status code - "Internal Server Error"
+});
 
 function handleServerListen() {
   console.log(`Server is listening on port ${port}`);
