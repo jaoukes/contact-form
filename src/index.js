750f4fcf0acb4d105ba9df3302d4f896078e0122
jessicaoukes <jessicaoukes@gmail.com>
2019-12-14 13:39:58 -0500

    Phase one on one

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
+app.use(express.json());
+
+
+
+
+
+
 function handleServerListen() {
   console.log(`Server is listening on port ${port}`);
 }
