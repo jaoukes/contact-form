548a762372c494d8a9dda75463fe1498580821bb
jessicaoukes <jessicaoukes@gmail.com>
2019-12-21 17:02:07 -0500

    Routes

--- server/index.js
+++ server/index.js
@@ -1,54 +1,60 @@
-'use strict';
+'use strict';
 
-let express = require('express');
+let express = require('express');
 
-let app = express();
-let port = 3000;
+let app = express();
+let port = 3000;
 
-// Middleware
-app.use(express.json()); // Allows to read JSON sent in 'req.body'
+// Middleware
+app.use(express.json()); // Allows to read JSON sent in 'req.body'
 
-// Route to create an entry when the user submits their form
-// TODO: Be more specific with your path, what IS is creating, also don't use verbs in paths, as paths should be nouns
-app.post('/create', async function (req, res, next) {
-  // I'd worry about validation later, the first thing you should after this route and the next route "works"
-  const formErrors = {};
-  if (!req.body.name) {
-    formErrors.name = 'Required';
-  }
-  if (!req.body.region) {
-    formErrors.region = 'Required';
-  }
+// Route to create an entry when the user submits their form
+// TODO: Be more specific with your path, what IS is creating, also don't use verbs in paths, as paths should be nouns
+app.post('/user/', async function (req, res, next) {
+  // I'd worry about validation later, the first thing you should after this route and the next route "works"
+//    const formErrors = {};
+//    if (!req.body.name) {
+//      formErrors.name = 'Required';
+//    }
+//    if (!req.body.region) {
+//      formErrors.region = 'Required';
+//    }
 });
 
-// Route to get a listing of all submissions
+// Route to get a listing of all submissions
 
 
-// Route to create or register a user
-// app.get('/create-user', function (req, res) {
-//   res.render('create-user', {
-//     formValues: {},
-//     formErrors: {},
-//   });
-// });
-
-// Route to log a registered user in to create a session
-// app.post('/create-user', async function (req, res, next) {
-//   const formErrors = {};
-//   if (!req.body.name) {
-//     formErrors.name = 'Required';
-//   }
-// });
-
-
-// Default Error Handler
-// This is always last!
-app.use(function (error, req, res, next) {
-  console.error(error); // this is here to see errors for debugging purposes
-  res.sendStatus(500); // Send 500 status code - "Internal Server Error"
+// Route to create or register a user
+app.post('user/create', async function (req, res, next){
+  response.send("User Registered");
 });
 
-function handleServerListen() {
-  console.log(`Server is listening on port ${port}`);
+// app.get('/create-user', function (req, res) {
+//   res.render('create-user', {
+//     formValues: {},
+//     formErrors: {},
+//   });
+// });
+
+// Route to log a registered user in to create a session
+
+// app.post('/create-user', async function (req, res, next) {
+//   const formErrors = {};
+//   if (!req.body.name) {
+//     formErrors.name = 'Required';
+//   }
+// });
+
+
+// Default Error Handler
+// This is always last!
+app.use(function (error, req, res, next) {
+  console.error(error); // this is here to see errors for debugging purposes
+  res.sendStatus(500); // Send 500 status code - "Internal Server Error"
+});
+
+function handleServerListen() {
+  console.log(`Server is listening on port ${port}`);
 }
-app.listen(port, handleServerListen);
+app.listen(port, handleServerListen);
+
