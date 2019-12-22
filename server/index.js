69eac9dc1624607594e66520cd5f2f31f8c40786
jessicaoukes <jessicaoukes@gmail.com>
2019-12-21 20:04:50 -0500

    route basics

--- server/index.js
+++ server/index.js
@@ -8,16 +8,41 @@ let port = 3000;
 // Middleware
 app.use(express.json()); // Allows to read JSON sent in 'req.body'
 
+
+
+
 // Submit form
 app.post('/contact_us/form', function (request, response, next) {
   response.send('User Form Submitted');
 });
 
+
+// Register a User
+app.post('/user/register', async function (request, response, next) {
+  response.send("User Registered");
+  console.log("User Registered")
+});
+
+
+// Log in a user (create session)
+app.post('/user/login', async function (request, response, next){
+  response.send("User Session");
+  console.log("User Session");
+});
+
+
+// Get a list of all submissions
+app.get('/contact_us/results', async function (request, response, next){
+  response.send();
+  console.log("All User Submissions");
+});
+
+
+
 // Default Error Handler
-// This is always last!
-app.use(function (error, req, res, next) {
-  console.error(error); // this is here to see errors for debugging purposes
-  res.sendStatus(500).send('Something broke!'); // Send 500 status code - "Internal Server Error"
+app.use(function (error, request, response, next) {
+  console.error(error);
+  response.sendStatus(500); // "Internal Server Error"
 });
 
 function handleServerListen() {
