e987f2b7611f819d152f9ae585706a2f4d8e7dc5
jessicaoukes <jessicaoukes@gmail.com>
2019-12-21 19:52:47 -0500

    submit form only

--- server/index.js
+++ server/index.js
@@ -10,24 +10,8 @@ app.use(express.json()); // Allows to read JSON sent in 'req.body'
 
 // Submit form
 app.post('/contact_us/form', function (request, response, next) {
-  console.log("User Form Submitted");
+  response.send('User Form Submitted');
 });
-
-// Get a list of all submissions
-app.get('/contact_us/results', function (request, response, next){
-  console.log("All User Submissions");
-});
-
-// Register a user
-app.post('/user/register', function (request, response, next) {
-  console.log("User Registered")
-});
-
-// Log in a user (create session)
-app.post('/user/login', function (request, response, next){
-  console.log("User Session");
-});
-
 
 // Default Error Handler
 // This is always last!
