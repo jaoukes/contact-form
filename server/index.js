0618878b67edca581d1d18b91cef7f4ec2fd369f
jessicaoukes <jessicaoukes@gmail.com>
2019-12-21 19:47:43 -0500

    index_origianl add

--- server/index.js
+++ server/index.js
@@ -8,31 +8,10 @@ let port = 3000;
 // Middleware
 app.use(express.json()); // Allows to read JSON sent in 'req.body'
 
-//let util = require('util');
-//let fs = require('fs');
-//let path = require('path');
-
-// Create versions of `fs` methods we'll be using to return promises
-//let readFile = util.promisify(fs.readFile);
-//let writeFile = util.promisify(fs.writeFile);
-
-// DB Path
-// let dbPath = path.resolve('server/db.json');
-
-
 // Submit form
-// TODO: Be more specific with your path, what IS is creating, also don't use verbs in paths, as paths should be nouns
 app.post('/contact_us/form', async function (request, response, next) {
   response.send();
   console.log("User Form Submitted");
-// I'd worry about validation later, the first thing you should after this route and the next route "works"
-//    const formErrors = {};
-//    if (!req.body.name) {
-//      formErrors.name = 'Required';
-//    }
-//    if (!req.body.region) {
-//      formErrors.region = 'Required';
-//    }
 });
 
 // Get a list of all submissions
@@ -47,24 +26,11 @@ app.post('/user/register', async function (request, response, next) {
   console.log("User Registered")
 });
 
-// app.get('/create-user', function (req, res) {
-//   res.render('create-user', {
-//     formValues: {},
-//     formErrors: {},
-//   });
-// });
-
 // Log in a user (create session)
 app.post('/user/login', async function (request, response, next){
   response.send("User Session");
   console.log("User Session");
 });
-// app.post('/create-user', async function (req, res, next) {
-//   const formErrors = {};
-//   if (!req.body.name) {
-//     formErrors.name = 'Required';
-//   }
-// });
 
 
 // Default Error Handler
