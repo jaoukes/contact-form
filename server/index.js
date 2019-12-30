7d8cca25c254599be192840928c8b3d01cbcce05
jessicaoukes <jessicaoukes@gmail.com>
2019-12-29 20:29:52 -0500

    submissions

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.post('/user/login', async function (request, response, next){
 // Get a list of all submissions
 app.get('/contact_us/results', async function (request, response, next) {
   let submissions = await db.submissions();
-  response.send(submissions);
+  response.send('Submissions:'submissions);
 });
 
 
